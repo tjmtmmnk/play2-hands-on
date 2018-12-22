c518ea6e8cc6c68a1d440fd409d497d7a825718a
TsujimotoTakumi <tsujimoto027@gmail.com>
2018-12-23 01:02:32 +0900

    feat: ルーティングを設定

--- app/controllers/UserController.scala
+++ app/controllers/UserController.scala
@@ -12,6 +12,22 @@ import javax.inject.Inject
 import scala.concurrent.Future
 import slick.driver.H2Driver.api._
 
-class UserController {
+/*
+* DatabaseConfigProvider : コントローラ内でDBにアクセスするため
+* MessageAPI : Playの国際化機能(日本語を使うか英語を使うかなど)を使用するため
+*              実際は使用しないが、テンプレート内で使用するヘルパーのために必要
+* I18nSupport : 国際化のために必要
+* @Inject : DI(依存性の注入)のためのアノテーション
+*/
+class UserController @Inject()(val dbCfgProvider: DatabaseConfigProvider, val msgApi: MessagesApi) extends Controller
+  with HasDatabaseConfigProvider[JdbcProfile] with I18nSupport {
+  def list = TODO
 
+  def edit(id: Option[Long]) = TODO
+
+  def create = TODO
+
+  def update = TODO
+
+  def remove(id: Long) = TODO
 }
