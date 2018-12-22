42a67a4882716a70c300e14d18f3e697fd62c5df
TsujimotoTakumi <tsujimoto027@gmail.com>
2018-12-23 00:38:16 +0900

    initial commit

---
+++ app/controllers/UserController.scala
@@ -0,0 +1,17 @@
+package controllers
+
+import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import play.api.i18n.{MessagesApi, I18nSupport}
+import play.api.db.slick._
+import slick.driver.JdbcProfile
+import models.Tables._
+import javax.inject.Inject
+import scala.concurrent.Future
+import slick.driver.H2Driver.api._
+
+class UserController {
+
+}
